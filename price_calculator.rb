654fffda31a0d4ebd13c6fd4aa850054497a34cc
David T. Allen <148455+bitlather@users.noreply.github.com>
2021-05-16 21:17:04 -0400

    Create a receipt hash when given a product and cart hash

--- price_calculator.rb
+++ price_calculator.rb
@@ -2,19 +2,23 @@
 # Usage:
 #------------------------------------------------------------------------------
 # $ ruby price_calculator.rb
-# $ ruby price_calculator.rb "Milk, milk,BREAD"
+# $ ruby price_calculator.rb "Milk, milk,BREAD,eggs"
 #==============================================================================
 require 'pp'
 
 def run(products)
   items = break_into_array(ask_for_items)
   cart = quantify(items)
+  receipt = checkout(products, cart)
 
   puts "CART:"
   pp cart
   puts
   puts "PRODUCTS:"
   pp products
+  puts
+  puts "RECEIPT:"
+  pp receipt
 end
 
 def ask_for_items
@@ -41,11 +45,64 @@ end
 
 def quantify(items)
   result = {}
+
   items.each do |item|
+
+    item = item.to_sym
+
     result[item] = 0 unless result.key? item
     result[item] += 1
+
   end
+
   result
+end
+
+def checkout(products, cart)
+  receipt = {
+    total_price: 0,
+    total_saved: 0,
+    items: [],
+    unrecognized: []    
+  }
+
+  cart.keys.sort.each do |key|
+
+    quantity = cart[key]
+
+    if !products.key? key
+      receipt[:unrecognized].push key
+      next
+    end
+
+    product = products[key]
+
+    item_total_price = quantity * product[:price]
+    item_discounted_price = item_total_price
+
+    if product.key? :sale
+      num_sales_applied = (quantity / product[:sale][:quantity]).floor
+      num_full_price = (num_sales_applied > 0) \
+        ? quantity % (num_sales_applied * product[:sale][:quantity]) \
+        : quantity
+
+      item_discounted_price = num_full_price * product[:price] + num_sales_applied * product[:sale][:price]
+    end
+
+    receipt[:total_price] += item_discounted_price
+    receipt[:total_saved] += (item_total_price - item_discounted_price)
+    # Note: For two milks, we get :total_saved=>2.9400000000000004, so maybe we should store
+    # the price of everything in cents to avoid floating point issues.
+
+    receipt[:items].push({
+      name: product[:name],
+      quantity: quantity,
+      total_price: item_total_price,
+      total_discounted_price: item_discounted_price })
+
+  end
+
+  receipt
 end
 
 run({
